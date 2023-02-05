8bd73bdc124cb311bf48eb2329292048f602a8c9
s-avery <s_avery@riseup.net>
2023-02-04 21:31:43 -0500

    wrapped up contact, put contact in works

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,24 @@
 import Header from "./Header";
 import placeholder from "../Assets/placeholder.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { computeHeadingLevel } from "@testing-library/react";
 
 const Contact = () => {
+	useEffect(() => {
+		setShowForm(false);
+	}, []);
+
 	// *State Zone
 	const [showForm, setShowForm] = useState(false);
 
 	const handleClick = () => {
+		// toggles state of if form is shown or not
 		setShowForm(!showForm);
+
+		// toggles the "animation" class that css uses to make the form slide in
+		const googleForm = document.querySelector(".contact__googleForm");
+		console.log(googleForm);
+		googleForm.classList.toggle("contact__googleForm--animation");
 	};
 
 	return (
@@ -26,21 +37,23 @@ const Contact = () => {
 							I’ll try to get back to you within a week, but
 							sometimes take longer.
 						</p>
-
 						<button className="button" onClick={handleClick}>
-							Show Form
+							{showForm ? "Hide Form" : "Show Form"}
 						</button>
 
-						{/* <iframe
-							src="https://docs.google.com/forms/d/e/1FAIpQLSecaknMnVD03p2U7etrIgxcHs6DJaSu1yeIlmtVsayjQaexiQ/viewform?embedded=true"
-							width="640"
-							height="673"
-							frameborder="0"
-							marginheight="0"
-							marginwidth="0"
-						>
-							Loading…
-						</iframe> */}
+						<div className="contact__googleFormContainer">
+							<iframe
+								src="https://docs.google.com/forms/d/e/1FAIpQLSecaknMnVD03p2U7etrIgxcHs6DJaSu1yeIlmtVsayjQaexiQ/viewform?embedded=true"
+								width="640"
+								height="800"
+								frameborder="0"
+								marginheight="0"
+								marginwidth="0"
+								className="contact__googleForm contact__googleForm--animation"
+							>
+								Loading…
+							</iframe>
+						</div>
 					</div>
 				</div>
 			</main>
--- src/Components/Works.js
+++ src/Components/Works.js
@@ -1,5 +1,7 @@
 import Header from "./Header";
-import placeholder from "../Assets/placeholder.png";
+import directed from "../Assets/directed.png";
+import edited from "../Assets/edited.jpeg";
+import essay from "../Assets/essay.png";
 
 const Works = () => {
 	return (
@@ -8,28 +10,66 @@ const Works = () => {
 
 			<main>
 				<div className="works__eyecatchers">
-					<h2>The Specials</h2>
+					<h2>The Hits</h2>
 					<div className="works__thumbnail">
-						<img src={placeholder} alt="" />
+						<a href="https://youtu.be/aLmgMjkOv7Q">
+							<img
+								src={essay}
+								alt="The thumbnail for Lily's Video 'Zoopraxography for Lovers (Cinema’s First Kiss Was Between Two Women)'. Sepia image of kissing against a background of the ocean, with text reading 'Who were they?'"
+							/>
+						</a>
 					</div>
 
 					<div className="works__thumbnail">
-						<img src={placeholder} alt="" />
+						<a href="https://youtu.be/BN2QenJZj1c">
+							<img
+								src={directed}
+								alt="The thumbnail for Lily's music video 'Pop Quiz' for Avery Lake. A woman in a basement holding her head at a weird angle, with glitchy video effects."
+							/>
+						</a>
 					</div>
 
 					<div className="works__thumbnail">
-						<img src={placeholder} alt="" />
+						<a href="https://www.thelowerplateau.com">
+							<img
+								src={edited}
+								alt="The poster for the movie The Lower Plateau. A woman sitting glumy on a bench with a light blue background, a floating red balloon, and text reading 'The Lower Plateau'."
+							/>
+						</a>
 					</div>
 				</div>
 
 				<div className="works__list">
-					<h2>Other Works</h2>
+					<h2>Selected Works</h2>
 
 					<div className="works__essays">
 						<h3>Video Essays</h3>
 						<ul>
 							<li>
-								<p>example</p>
+								<p>Fear of Trans Bodies (2022)</p>
+							</li>
+
+							<li>
+								<p>
+									Zoopraxography for Lovers (Cinema’s First
+									Kiss Was Between Two Women) (2022) --
+									Nominated for
+									<a href="https://www.bfi.org.uk/sight-and-sound/polls/best-video-essays-2022">
+										{" "}
+										best video essay 2022
+									</a>
+								</p>
+							</li>
+
+							<li>
+								<p>What Are Women? (2021)</p>
+							</li>
+
+							<li>
+								<p>
+									Millions of Dead Genders: a MOGAI
+									Retrospective (2021)
+								</p>
 							</li>
 						</ul>
 					</div>
@@ -38,7 +78,23 @@ const Works = () => {
 						<h3>Directed</h3>
 						<ul>
 							<li>
-								<p>example</p>
+								<p>
+									Trans Youth VS. Healthcare (3-part web
+									series, 2022)
+								</p>
+							</li>
+
+							<li>
+								<p>Avery Lake - Pop Quiz (music video, 2022)</p>
+							</li>
+							<li>
+								<p>
+									Shades Lawrence - Formidable Time (music
+									video, 2018)
+								</p>
+							</li>
+							<li>
+								<p>Red Cup Café (web series, 2017-2018)</p>
 							</li>
 						</ul>
 					</div>
@@ -47,7 +103,29 @@ const Works = () => {
 						<h3>Edited</h3>
 						<ul>
 							<li>
-								<p>example</p>
+								<p>Lia, 19 (short film, 2023)</p>
+							</li>
+
+							<li>
+								<p>
+									Ryan Stotland – Must Be Sigmund (music
+									video, 2021)
+								</p>
+							</li>
+							<li>
+								<p>
+									Ryan Stotland – That Kinda Freaked Me Out
+									(music video, 2021)
+								</p>
+							</li>
+							<li>
+								<p>
+									Absolutely Canadian: Absofreakinglutely
+									(comedy special, 2020)
+								</p>
+							</li>
+							<li>
+								<p>The Lower Plateau (feature film, 2018)</p>
 							</li>
 						</ul>
 					</div>
