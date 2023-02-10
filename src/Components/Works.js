77f3ede2eb92280c9b0345e47a15364184ed5cac
s-avery <s_avery@riseup.net>
2023-02-09 21:23:42 -0500

    links on works

--- src/Components/Works.js
+++ src/Components/Works.js
@@ -67,29 +67,56 @@ const Works = () => {
 						<h3>Video Essays</h3>
 						<ul>
 							<li>
-								<p>Fear of Trans Bodies (2022)</p>
+								<p>
+									<a
+										href="https://youtu.be/Yt2QPnhqIuI"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Fear of Trans Bodies (2022)
+									</a>
+								</p>
 							</li>
 
 							<li>
 								<p>
-									Zoopraxography for Lovers (Cinema’s First
-									Kiss Was Between Two Women) (2022) --
-									Nominated for
+									<a
+										href="https://youtu.be/aLmgMjkOv7Q"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Zoopraxography for Lovers (Cinema’s
+										First Kiss Was Between Two Women) (2022)
+									</a>{" "}
+									-- Nominated for{" "}
 									<a href="https://www.bfi.org.uk/sight-and-sound/polls/best-video-essays-2022">
-										{" "}
 										best video essay 2022
 									</a>
 								</p>
 							</li>
 
 							<li>
-								<p>What Are Women? (2021)</p>
+								<p>
+									<a
+										href="https://youtu.be/i6m3CzzYSOs"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										What Are Women? (2021)
+									</a>
+								</p>
 							</li>
 
 							<li>
 								<p>
-									Millions of Dead Genders: a MOGAI
-									Retrospective (2021)
+									<a
+										href="https://youtu.be/DoZFZto6Wqg"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Millions of Dead Genders: a MOGAI
+										Retrospective (2021)
+									</a>
 								</p>
 							</li>
 						</ul>
@@ -100,13 +127,28 @@ const Works = () => {
 						<ul>
 							<li>
 								<p>
-									Trans Youth VS. Healthcare (3-part web
-									series, 2022)
+									<a
+										href="https://youtube.com/playlist?list=PLJyGSoaq1Tt0BJ3YT1jt7ijx14TM6A8uh"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Trans Youth VS. Healthcare (3-part web
+										series, 2022)
+									</a>
 								</p>
 							</li>
 
 							<li>
-								<p>Avery Lake - Pop Quiz (music video, 2022)</p>
+								<p>
+									<a
+										href="https://youtu.be/BN2QenJZj1c"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Avery Lake - Pop Quiz (music video,
+										2022)
+									</a>
+								</p>
 							</li>
 							<li>
 								<p>
@@ -124,29 +166,59 @@ const Works = () => {
 						<h3>Edited</h3>
 						<ul>
 							<li>
-								<p>Lia, 19 (short film, 2023)</p>
+								<p>
+									<a
+										href="https://www.thelowerplateau.com/depanneur-films"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Lia, 19 (short film, 2023)
+									</a>
+								</p>
 							</li>
 
 							<li>
 								<p>
-									Ryan Stotland – Must Be Sigmund (music
-									video, 2021)
+									<a
+										href="https://youtu.be/P8g0Tad-rjg"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Ryan Stotland – Must Be Sigmund (music
+										video, 2021)
+									</a>
 								</p>
 							</li>
+
 							<li>
 								<p>
 									Ryan Stotland – That Kinda Freaked Me Out
 									(music video, 2021)
 								</p>
 							</li>
+
 							<li>
 								<p>
-									Absolutely Canadian: Absofreakinglutely
-									(comedy special, 2020)
+									<a
+										href="https://gem.cbc.ca/media/absolutely-canadian/s20e04"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										Absolutely Canadian: Absofreakinglutely
+										(comedy special, 2020)
+									</a>
 								</p>
 							</li>
 							<li>
-								<p>The Lower Plateau (feature film, 2018)</p>
+								<p>
+									<a
+										href="https://www.thelowerplateau.com/"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										The Lower Plateau (feature film, 2018)
+									</a>
+								</p>
 							</li>
 						</ul>
 					</div>
