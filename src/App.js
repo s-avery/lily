01822252014e8b5bdd64709512d94313ab6b1821
s-avery <s_avery@riseup.net>
2023-03-05 11:36:48 -0500

    error page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import "./Styles/App.scss";
 import Home from "./Components/Home";
 import Works from "./Components/Works";
 import Contact from "./Components/Contact";
+import ErrorPage from "./Components/ErrorPage";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
-			`<Routes>
+			`
+			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/Works" element={<Works />} />
 				<Route path="/Contact" element={<Contact />} />
+				<Route path="*" element={<ErrorPage />} />
 			</Routes>
 		</div>
 	);
--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,12 +1,9 @@
+import { Link } from "react-router-dom";
+
 const ErrorPage = () => {
 	return (
 		<main className="error">
-			<Link to="/" className="homeButton">
-				home
-			</Link>
-
 			<h2>this should not have happened.</h2>
-			<img src={angy} alt="angry paisley pudge" className="angy" />
 			<p className="error__text">
 				you're on a page that doesn't exist. go{" "}
 				<Link to="/" className="error__text">
