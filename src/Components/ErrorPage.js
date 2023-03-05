0578895c5481626c2f51a22bd671afb3eb5f9d4e
s-avery <s_avery@riseup.net>
2023-03-05 13:36:29 -0500

    header in error page

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,17 +1,20 @@
 import { Link } from "react-router-dom";
-
+import Header from "./Header";
 const ErrorPage = () => {
 	return (
-		<main className="error">
-			<h2>this should not have happened.</h2>
-			<p className="error__text">
-				you're on a page that doesn't exist. go{" "}
-				<Link to="/" className="error__text">
-					back
-				</Link>
-				.
-			</p>
-		</main>
+		<>
+			<Header />
+			<main className="error">
+				<h2>this should not have happened.</h2>
+				<p className="error__text">
+					you're on a page that doesn't exist. go{" "}
+					<Link to="/" className="error__text">
+						back
+					</Link>
+					.
+				</p>
+			</main>
+		</>
 	);
 };
 
